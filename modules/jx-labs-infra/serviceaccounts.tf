d2782e1d178f0c4a0d259f8204ddbfb0fa13380e
James Rawlings <rawlingsj80@gmail.com>
2021-04-23 13:01:10 +0100

    chore: enable cosign again 3

--- modules/jx-labs-infra/serviceaccounts.tf
+++ modules/jx-labs-infra/serviceaccounts.tf
@@ -1,6 +1,13 @@
-resource "google_project_iam_member" "tekton_sa_storage_object_admin_binding" {
+resource "google_project_iam_member" "tekton_sa_kms_decrypter" {
   provider = google
-  role     = "roles/cloudkms.cryptoKeyVersions.useToSign"
+  role     = "roles/cloudkms.cryptoKeyDecrypter"
+  member   = "serviceAccount:${var.tekton_sa_email}"
+  project = "jx-labs-infra"
+}
+
+resource "google_project_iam_member" "tekton_sa_kms_signer" {
+  provider = google
+  role     = "roles/cloudkms.signer"
   member   = "serviceAccount:${var.tekton_sa_email}"
   project = "jx-labs-infra"
 }
